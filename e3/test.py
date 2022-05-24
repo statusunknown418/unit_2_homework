918e5d7595abd7bb623a5ec04266ea45fe9c1c29
alvaro <alvarodevcode@outlook.com>
2022-05-23 22:48:26 -0500

    feat: added better code splitting

--- e3/test.py
+++ e3/test.py
@@ -1,5 +1,7 @@
 import pandas
 
+from __helpers import (employee_options, professor_options, quit_program,
+                       student_options)
 from Employee import Employee
 from Professor import Professor
 from Student import Student
@@ -9,58 +11,6 @@ regular_employees = []
 professors = []
 
 students = []
-
-
-def quit_program(groups: list[list] = []):
-    if len(groups) == 0:
-        print("Program exited successfully!")
-        exit()
-
-    df = pandas.DataFrame(columns=["Name", "Last Name", "Address", "Postal/Zip Code",
-                          "Phone Number", "SSN", "Salary", "Department", "Is Member"])
-
-    # Merge all groups into one dataFrame
-    for group in groups:
-        df = df.append(pandas.DataFrame(group, index=[0]))
-
-    print("Report generated:")
-    print(df)
-
-    df.to_csv("report.csv", index=False)
-    exit()
-
-
-def employee_options():
-    return {
-        "name": input("Name: "),
-        "last_name": input("Last Name: "),
-        "address": input("Address: "),
-        "postal_code": input("Postal/Zip Code: "),
-        "phone_number": input("Phone Number: "),
-        "ssn": input("SSN: "),
-        "salary": input("Salary: "),
-        "department": input("Department: "),
-    }
-
-
-def professor_options():
-    options = employee_options()
-    return {
-        **options,
-        "ssn": input("SSN: "),
-        "salary": input("Salary: "),
-        "department": input("Department: "),
-        "is_member": input("Is Member: Y/N").upper() == "Y",
-    }
-
-
-def student_options():
-    options = employee_options()
-    return {
-        **options,
-        "career": input("Career: "),
-        "grades_average": input("Grades Average: "),
-    }
 
 
 # The option `keys` are not in English (doesn't follow good practices) because that's what's asked
